3ba8acc9c290d3dc65c84b99f42a59b19c67c00b
Ben_Hsu <ben_hsu@ubiqconn.com>
2020-12-25 11:48:10 +0800

    加入專案檔案。

---
+++ QSoft.Exif/ExifReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSoft.Exif
+{
+    public class ExifReader
+    {
+        public bool Open(Stream stream)
+        {
+            BinaryReader br = new BinaryReader(stream);
+
+
+
+            br.Close();
+            br.Dispose();
+            return true;
+        }
+    }
+}
